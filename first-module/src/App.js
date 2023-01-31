c56600e4dd6e9d591c383728134c10fc2fae51d9
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-01 00:41:25 +0100

    iterate through a list with .map

--- first-module/src/App.js
+++ first-module/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 
 function App() {
+  const names = ["Pedro", "Jake", "Jessica", "Sarah"];
+
   return (
     <div className="App">
       <Job salary={90000} position="Senior SDE" company="Amazon" />
       <Job salary={12000} position="Junior SDE" company="Google" />
       <Job salary={10000} position="Project Manager" company="Netflix" />
+
+      {names.map((value, key) => {
+        return <h1 key={key}>Hello {value}</h1>
+      })}
     </div>
   );
 }
