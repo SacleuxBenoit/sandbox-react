3b71a6ccc4d5bcf1b994a69dcc1c5d967a20ed8d
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-01-31 19:46:02 +0100

    add test with jsx && props

--- first-module/src/App.js
+++ first-module/src/App.js
@@ -2,8 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Hello world</div>
+    <div className="App">
+      <User name="pierre" age={42} surname="rock"/>
+      <User name="paul" age={12} surname="popo"/>
+      <User name="jack" age={23} surname="jack"/>
+    </div>
   );
 }
 
+const User = (props) => {
+  return(
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+      <h3>{props.surname}</h3>
+    </div>
+  );
+};
 export default App;
