80563b1e169260256be11ff3a74b06750419209f
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-09 15:45:07 +0100

    add inputChange

--- first-module/src/App.js
+++ first-module/src/App.js
@@ -2,29 +2,18 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let [number, setNumber] = useState(0)
+  let [inputValue, setInputValue] = useState("");
 
-  let increaseNumber = () => {
-    setNumber(number + 1)
-}
+  let handleInputChange = (e) => {
+    setInputValue(e.target.value)
+    console.log(e.target.value)
+  }
+
   return (
     <div className="App">
-      {number}
-      <button onClick={increaseNumber}>Click to increase</button>
-      {/* {planets.map((planet,key) => {
-        if(planet.isGasPlanet){
-          return <h1>{planet.name}</h1>
-        }
-      })} */}
+      <input type="text" onChange={handleInputChange}/>
+      {inputValue}
     </div>
   );
 }
-
-// const Job = (props) => {
-//   return(
-//     <div>
-//     <p>The company is {props.company} your position will be {props.position} and the salary is {props.salary} dollars </p>
-//   </div>
-//   );
-// };
 export default App;
