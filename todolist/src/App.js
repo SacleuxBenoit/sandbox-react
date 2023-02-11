868df9cfcf8457b14852f412e64aa934f915cab8
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-11 16:24:30 +0100

    display tasks

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { useState } from 'react' 
 
 function App() {
+  let [todoList, setTodolist] = useState([]);
+  let [newTask, setNewTask] = useState("");
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  }
+
+  let addTask = () => {
+    let newTodolist = [...todoList, newTask];
+    setTodolist(newTodolist);
+    // setTodolist([...todoList, newTask]); Work same way as above
+  }
   return (
     <div className="App">
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add task</button>
+      </div>
 
+      <div className="list">
+        {todoList.map((task) => {
+          return(
+            <h1>{task}</h1>
+          )
+        })}
+      </div>
     </div>
   );
 }
