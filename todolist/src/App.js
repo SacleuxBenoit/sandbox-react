e1efcc3b1fe73f9c72cbe460d18a613ba0f461c0
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-12 22:49:31 +0100

    change color of tasks depand of if they are completed or not

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -13,12 +13,25 @@ function App() {
   let addTask = () => {
     const task = {
       id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask
+      taskName: newTask,
+      isCompleted: false
     };
 
     let newTodolist = [...todoList, task];
     setTodolist(newTodolist);
     // setTodolist([...todoList, newTask]); Work same way as above
+  }
+
+  let completeTask = (id) => {
+    setTodolist(
+      todoList.map((task) => {
+        if(task.id === id){
+          return{...task, isCompleted: true}
+        }else{
+          return task
+        }
+      })
+    )
   }
 
   let deleteTask = (id) => {
@@ -38,7 +51,10 @@ function App() {
             <Task 
               taskName={task.taskName}
               id={task.id}
-              deleteTask={deleteTask}/>
+              deleteTask={deleteTask}
+              completTask={completeTask}
+              isCompleted={task.isCompleted}
+            />
           )
         })}
       </div>
--- todolist/src/Task.js
+++ todolist/src/Task.js
@@ -1,7 +1,8 @@
 export let Task = (props) => {
     return(
-        <div>
+        <div className="task" style={{ backgroundColor: props.isCompleted ? "green" : "white"}}>
           <h1>{props.taskName}</h1>
+          <button onClick={() => props.completTask(props.id)}>complete</button>
           <button onClick={() => props.deleteTask(props.id)}>x</button>
         </div>
       )
