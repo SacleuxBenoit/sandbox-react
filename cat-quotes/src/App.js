bce8a6415ab1041c1634fffac1b42a00afec3138
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-14 17:02:33 +0100

    fetch api && display some facts about cat

--- cat-quotes/src/App.js
+++ cat-quotes/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Axios from "axios";
+import { useState, useEffect } from "react"
 
 function App() {
+  let [catFacts, setCatFacts] = useState("");
+
+  let fetchCatFacts = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFacts(res.data.fact)
+    })
+  }
+  useEffect(() => {
+    fetchCatFacts();
+  }, [])
+
   return (
     <div className="App">
-      
+      <button onClick={fetchCatFacts}>Generate cat fact</button>
+      <p>{catFacts}</p>
     </div>
   );
 }
