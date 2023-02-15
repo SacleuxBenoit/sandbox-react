fa57b004ffae1c1245b8598637e1f33ce0e591e5
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-15 17:16:25 +0100

    display stats from an api based on firstname

--- predict-age/src/App.js
+++ predict-age/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import Axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  let [username, setUsername] = useState("")
+  let [predictedAge, setPredictedAge] = useState(null);
+
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${username}`).then((res) => {
+      setPredictedAge(res.data)
+    })
+  }
   return (
     <div className="App">
+      <input 
+        placeholder="exemple : Benoît"
+        onChange={(event) => {setUsername(event.target.value)}} 
+      />
+      <button onClick={fetchData}>Predict age</button>
 
+      <h1>Name : {predictedAge?.name}</h1>
+      <h1>Count : {predictedAge?.count}</h1>
+      <h1>Predicted age : {predictedAge?.age}</h1>
     </div>
   );
 }
