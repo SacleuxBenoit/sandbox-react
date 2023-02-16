fc878a01ce5bb7c874eacf17e0dea13484392758
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-16 17:52:09 +0100

    create components NavBar + call him in App.js

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Menu } from './pages/Menu'
 import { Contact } from './pages/Contact'
+import {NavBar} from './pages/NavBar'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/menu">Menu</Link>
-          <Link to="/contact">Contact</Link>
-        </div>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/menu" element={<Menu />}/>
