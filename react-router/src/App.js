1b97370b361724b8579ff6d9c8d876ae4f2ba4a5
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-16 17:48:34 +0100

    add navbar with (link)

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Menu } from './pages/Menu'
 import { Contact } from './pages/Contact'
@@ -8,10 +8,16 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/menu" element={<Menu />}/>
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </Router>
     </div>
