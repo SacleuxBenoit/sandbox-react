c62b5f7047fa50d9b8fc73225452d5a9deefb7d4
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-17 18:27:59 +0100

    fetch data with react-query

--- react-query/src/App.js
+++ react-query/src/App.js
@@ -6,25 +6,33 @@ import { Menu } from './pages/Menu'
 import { Contact } from './pages/Contact'
 import { NavBar } from './pages/NavBar'
 import { useState, createContext } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export let AppContext = createContext();
 function App() {
+  let client = new QueryClient({defaultOptions:{
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+  }});
   let [username, setUsername] = useState("Benoît");
 
   return (
     <div className="App">
-      <AppContext.Provider value={{username, setUsername}}>   
-        <Router>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Home username={username}/>}/>
-            <Route path="/profile" element={<Profile/>} />
-            <Route path="/menu" element={<Menu/>}/>
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<Home />} />
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+      <QueryClientProvider client={client}>
+        <AppContext.Provider value={{username, setUsername}}>   
+          <Router>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home username={username}/>}/>
+              <Route path="/profile" element={<Profile/>} />
+              <Route path="/menu" element={<Menu/>}/>
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<Home />} />
+            </Routes>
+          </Router>
+        </AppContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }
--- react-query/src/pages/Home.js
+++ react-query/src/pages/Home.js
@@ -1,7 +1,20 @@
 import { useContext } from 'react' 
 import { AppContext } from '../App'
+import { useQuery } from '@tanstack/react-query'
+import Axios from "axios";
 
 export let Home = () => {
+    let { data, isLoading, refetch } = useQuery(["cat"], () => {
+        return Axios.get("https://catfact.ninja/fact").then((res) => res.data)
+    });
+
     const {username} = useContext(AppContext)
-    return <h1>Hello {username} you are in the Home page</h1>
+    
+    return(
+        <div>
+            <h1>Hello {username} you are in the Home page</h1>
+            {isLoading ? <p>api is loading</p> : <p>{data?.fact}</p>}
+            <button onClick={refetch}>Update data</button>
+        </div>
+    )
 }
