afb4defd3f50054aa4764932bc491001cfead25a
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-08-16 13:57:46 +0200

    display / hide text when we click on button in :arrow_right: HideShow.js && create Count.js in pages folder

--- react_course/src/pages/HideShow.js
+++ react_course/src/pages/HideShow.js
@@ -1,9 +1,18 @@
- import NavBar from "../components/NavBar"
+ import { useState } from "react"
+import NavBar from "../components/NavBar"
 
 export default function HideShow(){
+    let [showText, setShowText] = useState(true)
+
+    function changeStateShowText(){
+        showText === true ? setShowText(false) : setShowText(true)
+    }
+
     return(
         <>
             <NavBar />
+            <button onClick={changeStateShowText}>Show / Hide</button>
+            {showText === true && <h1>We can see this text</h1>}
         </>
     ) 
 }
