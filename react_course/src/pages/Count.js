0bc4cdfb404a4cecb46f5f2a8526774c5959d576
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-08-16 14:07:19 +0200

    :memo: increase, decrease && set count to 0

--- react_course/src/pages/Count.js
+++ react_course/src/pages/Count.js
@@ -1,11 +1,17 @@
+import { useState } from "react"
 import NavBar from "../components/NavBar"
 
 export default function Count(){
+    let [count, setCount] = useState(0);
+
     return(
         <>
             <NavBar />
             <div>
-                Hey from count
+                <button onClick={() => setCount(count += 1)}>Increase</button>
+                <button onClick={() => setCount(count -= 1)}>Decrease</button>
+                <button onClick={() => setCount(0)}>Reset</button>
+                {count}
             </div>
         </>
     )
