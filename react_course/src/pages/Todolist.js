2551ec4e06c8c19796a91c3141737c39e19c0bfd
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-08-21 15:51:48 +0200

    add new task into array and display them

--- react_course/src/pages/Todolist.js
+++ react_course/src/pages/Todolist.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import NavBar from "../components/NavBar";
 
 export default function Todolist(){
+    const [todolist, setTodolist] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    const handleChange = (event) => {
+        setNewTask(event.target.value);
+    } 
+
+    const addTask = () => {
+        const newTodolist = [...todolist, newTask]
+        setTodolist(newTodolist);
+    }
+
     return(
         <>
             <NavBar />
-            Hey from Todolist
+            <div className="addTask">
+                <input onChange={handleChange}/>
+                <button onClick={addTask}>Add task</button>
+            </div>
+
+            <div className="list">
+                {todolist.map((task) => {
+                    return(<h1>{task}</h1>)
+                })}
+            </div>
         </>
     )
 }
