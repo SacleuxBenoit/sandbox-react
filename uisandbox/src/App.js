3eb238a6de266e2176fe8a635a3b39a189efb955
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-12-22 00:07:20 +0100

    create file Home.js && add Route to it

--- uisandbox/src/App.js
+++ uisandbox/src/App.js
@@ -1,6 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
