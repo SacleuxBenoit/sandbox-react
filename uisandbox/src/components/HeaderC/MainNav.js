53dafaf16b0f811805e7c5ffe13c64620f0458c7
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-12-28 15:46:17 +0100

    add possibility to close the leftNavbar

--- uisandbox/src/components/HeaderC/MainNav.js
+++ uisandbox/src/components/HeaderC/MainNav.js
@@ -6,13 +6,9 @@ import LeftNavBar from "../LeftNavBar"
 function MainNav() {
     const [navLeft, setNavLeft] = useState(false)
 
-    function toggleLeftNav() {
-        setNavLeft(!navLeft)
-    }
-
     return (
         <>
-            <button onClick={toggleLeftNav}>Tous</button>
+            <button onClick={() => { setNavLeft(!navLeft) }}>Tous</button>
             <Link to="/">Carte cadeaux</Link>
             <Link to="/">Nos meilleurs ventes</Link>
             <Link to="/">Vente flash</Link>
@@ -20,7 +16,7 @@ function MainNav() {
             <Link to="/">Livre</Link>
             <Link to="/">Informatique</Link>
             <Link to="/">Audio</Link>
-            {navLeft && <LeftNavBar />}
+            {navLeft && <LeftNavBar closeNavLeft={() => { setNavLeft(false) }} />}
         </>
     )
 }
--- uisandbox/src/components/LeftNavBar.js
+++ uisandbox/src/components/LeftNavBar.js
@@ -1,42 +1,45 @@
 import '../styles/LeftNavBar.css'
 import { Link } from 'react-router-dom'
 
-function LeftNavBar() {
+function LeftNavBar({ closeNavLeft }) {
     return (
-        <div className="containerNavLeft">
-            <Link to="/"><div className='identification'>Bonjour, identifiez-vous</div></Link>
-            <div>
-                <ul>
-                    <li className="menuTitle">Tendances</li>
-                    <Link to="/"><li>Meilleurs ventes</li></Link>
-                    <Link to="/"><li>Dernières Nouveautés</li></Link>
-                    <Link to="/"><li>Baromètre des ventes</li></Link>
+        <div className='wrapper'>
+            <div className="containerNavLeft">
+                <Link to="/"><div className='identification'>Bonjour, identifiez-vous</div></Link>
+                <div>
+                    <ul>
+                        <li className="menuTitle">Tendances</li>
+                        <Link to="/"><li>Meilleurs ventes</li></Link>
+                        <Link to="/"><li>Dernières Nouveautés</li></Link>
+                        <Link to="/"><li>Baromètre des ventes</li></Link>
 
-                    <li className="menuSeparator"></li>
+                        <li className="menuSeparator"></li>
 
-                    <li className="menuTitle">Contenu Numérique et Appareils</li>
-                    <Link to="/"><li>Vidéo</li></Link>
-                    <Link to="/"><li>Musique</li></Link>
-                    <Link to="/"><li>Maison connectée</li></Link>
-                    <Link to="/"><li>Kindle & ebooks</li></Link>
-                    <Link to="/"><li>Livre audio</li></Link>
+                        <li className="menuTitle">Contenu Numérique et Appareils</li>
+                        <Link to="/"><li>Vidéo</li></Link>
+                        <Link to="/"><li>Musique</li></Link>
+                        <Link to="/"><li>Maison connectée</li></Link>
+                        <Link to="/"><li>Kindle & ebooks</li></Link>
+                        <Link to="/"><li>Livre audio</li></Link>
 
-                    <li className="menuSeparator"></li>
+                        <li className="menuSeparator"></li>
 
-                    <li className="menuTitle">Choisir une catégorie</li>
-                    <Link to="/"><li>Livres</li></Link>
-                    <Link to="/"><li>Musique, DvD</li></Link>
-                    <Link to="/"><li>Jeux Vidéo et console</li></Link>
-                    <Link to="/"><li>High-tech</li></Link>
+                        <li className="menuTitle">Choisir une catégorie</li>
+                        <Link to="/"><li>Livres</li></Link>
+                        <Link to="/"><li>Musique, DvD</li></Link>
+                        <Link to="/"><li>Jeux Vidéo et console</li></Link>
+                        <Link to="/"><li>High-tech</li></Link>
 
-                    <li className="menuSeparator"></li>
+                        <li className="menuSeparator"></li>
 
-                    <li className='menuTitle'>Programmes et services</li>
-                    <Link to="/"><li>Launchpad</li></Link>
-                    <Link to="/"><li>Fabriqué en France</li></Link>
-                    <Link to="/"><li>Essayez avant d'acheter</li></Link>
-                </ul>
+                        <li className='menuTitle'>Programmes et services</li>
+                        <Link to="/"><li>Launchpad</li></Link>
+                        <Link to="/"><li>Fabriqué en France</li></Link>
+                        <Link to="/"><li>Essayez avant d'acheter</li></Link>
+                    </ul>
+                </div>
             </div>
+            <div className='divButtonClose' onClick={closeNavLeft}>x</div>
         </div>
     )
 }
